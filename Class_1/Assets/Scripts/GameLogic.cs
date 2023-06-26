43d552f02513f3163ea4b42744b5e38aca0aed40
Leo <leoamedu14@gmail.com>
2023-06-26 18:26:06 +0200

    improved game logic

--- Class_1/Assets/Scripts/GameLogic.cs
+++ Class_1/Assets/Scripts/GameLogic.cs
@@ -6,11 +6,24 @@ public class GameLogic : MonoBehaviour
 {
     public GameObject Collectable;
     public GameObject BadCollectable;
-    
+
+    private float randomXCollectable = 0f;
+    private float randomYCollectable = 0f;
+
+    private float randomXBadCollectable = 0f;
+    private float randomYBadCollectable = 0f;
+
+    private float collectableSpawnTime = 1f;
+    private float badCollectableSpawnTime = 1f;
+
+    private float maxX = 17.5f;
+    private float maxY = 9f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(SpawnCollectables());
+        StartCoroutine(SpawnBadCollectables());        
     }
 
     // Update is called once per frame
@@ -18,4 +31,39 @@ public class GameLogic : MonoBehaviour
     {
         
     }
+
+    IEnumerator SpawnCollectables()
+    {
+        while(true)
+        {
+            SpawnCollectable();
+            yield return new WaitForSeconds(collectableSpawnTime);
+        }
+    }
+
+    IEnumerator SpawnBadCollectables()
+    {
+        while(true)
+        {
+            SpawnBadCollectable();
+            yield return new WaitForSeconds(badCollectableSpawnTime);
+        }
+    }
+
+    void SpawnCollectable()
+    {
+        randomXCollectable = Random.Range(-maxX, maxX);
+        randomYCollectable = Random.Range(-maxY, maxY);
+
+        Instantiate(Collectable, new Vector2(randomXCollectable, randomYCollectable), Quaternion.identity);
+    } 
+
+    void SpawnBadCollectable()
+    {
+        randomXBadCollectable = Random.Range(-maxX, maxX);
+        randomYBadCollectable = Random.Range(-maxY, maxY);
+
+        Instantiate(BadCollectable, new Vector2(randomXBadCollectable, randomYBadCollectable), Quaternion.identity);
+    } 
+
 }
